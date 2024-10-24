4b012473143560c6fe8b05577f7154b7359ae59d
Rayyan <113837424+rayyan-sheikh@users.noreply.github.com>
2024-10-24 20:01:50 +0530

    localhost changes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"; // Import cors
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -14,10 +13,8 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
-
-// Enable CORS for all origins
-app.use(cors()); // Use cors middleware
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
